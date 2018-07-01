455483277b7c484f030b1968d5fe67c6d78749e9
mnqumenilungile <40722210+mnqumenilungile@users.noreply.github.com>
2018-07-01 06:50:05 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,36 +1,36 @@
 (function() {
   'use strict';
 
-  // set global variables
+  // set variables
   const $colorPicker = document.getElementById("colorPicker");
   const $sizePicker = document.getElementById("sizePicker");
   const $table = document.getElementById("pixel_canvas");
 
-  // add listener to select grid size
+  // add size listener
   $sizePicker.addEventListener('submit', function() {
     // prevent page refresh on submit
     event.preventDefault();
 
-    // get input data and draw grid
+    // input data in draw grid
     let width = document.getElementById("input_width").value;
     let height = document.getElementById("input_height").value;
     makeGrid(width, height);
   })
 
-  // Draw grid
+  // grid
   function makeGrid(width, height) {
     $table.innerHTML = '';
-    for (let row = 0; row < width; row++) {
+    for (let row = 1; row < width; row++) {
       let newRow = $table.insertRow();
         for (let cell= 0; cell < height; cell++ ) {
-          // add new cell with listener to change color
+          // add cell color
           let newCell = newRow.insertCell();
           newCell.onclick = changeColor;
         }
     }
   }
 
-  // change the color of the clicked cell to current color
+  // change color listener if clicked
   function changeColor() {
     this.style.background = $colorPicker.value;
   }
